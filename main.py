5ff28963102533140b80ffdb8ade3174d62aefd9
Mourad-Amj <mourad.amjahed@outlook.com>
2023-06-05 10:05:48 +0200

    g

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Union, Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, ValidationError
+
+app = FastAPI()
+
+class Data(BaseModel):
+    salary: int
+    bonus: int
+    taxes: int
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/double/{num}")
+def double(num: int):
+    return {"result": num * 2}
+
+@app.post("/compute")
+def compute(data: Data):
+    try:
+        result = data.salary + data.bonus - data.taxes
+        return {"result": result}
+    except ValidationError as e:
+        return {"error": "expected numbers, got strings."}
